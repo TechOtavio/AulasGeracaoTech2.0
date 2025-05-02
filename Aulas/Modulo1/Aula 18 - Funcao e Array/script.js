357f573a3e2db897c748fa42be8b2a23b77c1654
Otávio Levi <otavio.holanda05@gmail.com>
2025-05-02 08:23:09 -0300

    Adicionando aula 18

---
+++ Aulas/Modulo1/Aula 18 - Funcao e Array/script.js
@@ -0,0 +1,35 @@
+// Arrow function
+// const ConverterTemp = (C) => {
+//     let F = (C * 9/5) + 32;
+//     return F;
+// }
+// console.log(`A temperatura em F é ${ConverterTemp(10)}`)
+
+
+// Arrays
+// let nomes = ['Ola', 'mundo', '!', 10, 20, 30, true, true, false]
+// console.log(nomes[0])
+// nomes[1] = 'World'
+
+// console.log(nomes)
+// console.log(nomes.length)
+
+// let notas = []
+// let media = 0
+// for(let i = 0; i < 3; i++){
+//     let nota = Number(prompt(`Digite sua nota ${i+1}: `))
+//     notas[i] = nota
+//     media += nota
+// }
+// console.log(notas)
+// console.log(`Sua média é ${(media/notas.length).toFixed(2)}`)
+
+// let nomes = []
+// for(let i = 0; i < 3; i++){
+//     let nome = prompt(`Informe o ${i+1} nome: `)
+//     nomes[i] = nome
+//     console.log(`Olá ${nomes[i]}, seja bem-vindo!`)
+// }
+
+
+//Metodos de Arrrays
